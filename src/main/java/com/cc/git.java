0068133af1e6135afcc5eb44ebf304b7f8f95c0c
孙希鹏 <sxp52041@163.com>
2021-01-18 17:00:04 +0800

    创建

---
+++ src/main/java/com/cc/git.java
@@ -0,0 +1,11 @@
+package com.cc;
+
+/**
+ * @author SXP
+ * @date 2021/1/18
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaa");
+    }
+}
